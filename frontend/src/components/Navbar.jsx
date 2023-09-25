4291069f4339c755307bd03cfd9e3eb88ae29686
atahtee <atatisam14@gmail.com>
2023-09-25 23:54:26 +0300

    To Bloggy

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   const {user} = useContext(UserContext);
   return (
     <div className='flex items-center justify-between px-6 md:px-[200px] py-4'>
-      <h1 className='text-lg md:text-xl font-extrabold'><Link to="/">BlogWise</Link></h1>
+      <h1 className='text-lg md:text-xl font-extrabold'><Link to="/">Bloggy</Link></h1>
     {path === "/" && <div className='flex justify-center items-center space-x-0'>
         <p onClick={()=>navigate(prompt?"?search="+prompt:navigate("/"))} className='cursor-pointer'><BsSearch /></p>
         <input onChange={(e)=>setPrompt(e.target.value)} className='outline-none px-3 py-1' placeholder='Search a post' type='text' />
