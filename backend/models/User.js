66a32ca0afd37f2a1eb01d49b8232ee1e2e23275
atahtee <atatisam14@gmail.com>
2023-10-11 14:35:37 +0300

    Second Commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
     email:{
         type: String,
         required: true,
-        unique:true
+        unique:true,
     },
     password:{
         type: String,
--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -21,7 +21,7 @@ const Menu = () => {
     }
   return (
     <div className='bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4'>
-        {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to="/login">Login</Link></h3>}
+        {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to="/login">  Login</Link></h3>}
         {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to="/register">Register</Link></h3>}
         {user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to={"/profile/"+user._id}>Profile</Link></h3>}
         {user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to="/write">Write</Link></h3>}
