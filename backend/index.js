8aefff5a4269a3eda624d3757882603bd1a7416f
atahtee <atatisam14@gmail.com>
2023-10-21 01:03:09 +0300

    Changed origin

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors(
     {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5174"],
     methods: ["POST", "GET"],
     credentials:true}
     ))
