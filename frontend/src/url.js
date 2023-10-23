db9ce23f29a8fbe6bf5b0f1054bf2014e79f8449
atahtee <atatisam14@gmail.com>
2023-10-23 08:35:49 +0300

    Changed URL.JS

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -2,6 +2,8 @@
 // export const IF = "https://2blog-atahtee.vercel.app/images"
 
 
-export const URL="http://localhost:5000"
-export const IF="http://localhost:5000/images/"
+// export const URL="http://localhost:5000"
+// export const IF="http://localhost:5000/images/"
 
+export const URL = "https://tublog-backend.onrender.com"
+export const IF = "http://localhost:5000/images/"
