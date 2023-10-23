917c17a7ec9a777956aa957901a6ada03a8e50b6
atahtee <atatisam14@gmail.com>
2023-10-23 08:30:16 +0300

    Changed Url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,8 +1,7 @@
-export const URL = "https://2blog-atahtee.vercel.app"
-export const IF = "https://2blog-atahtee.vercel.app/images"
+// export const URL = "https://2blog-atahtee.vercel.app"
+// export const IF = "https://2blog-atahtee.vercel.app/images"
 
 
-// export const URL="http://localhost:5000"
-// export const IF="http://localhost:5000/images/"
+export const URL="http://localhost:5000"
+export const IF="http://localhost:5000/images/"
 
-// I don't know if this 
