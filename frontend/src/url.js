3532ac4784865fc5ba39aaf9b5781e572fe5a183
atahtee <atatisam14@gmail.com>
2023-10-26 21:44:56 +0300

    Changed url js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,7 +1,7 @@
-export const URL="http://localhost:5000"
+export const URL="https://tublog-backend.onrender.com"
 // export const IF="http://localhost:5000/images/"
 
 // export const URL = "https://tublog-backend.onrender.com"
-export const IF = "http://localhost:5000/images/"
+export const IF = "https://tublog-backend.onrender.com/images/"
 
 //returns a value
