2fb31f40b291c43c338aaadaccdcf4522a2190f2
atahtee <atatisam14@gmail.com>
2023-10-30 23:25:45 +0300

    Changed url

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,8 +1,8 @@
-// export const URL="https://tublog-backend.onrender.com"
-// // export const IF="http://localhost:5000/images/"
+export const URL="https://tublog-backend.onrender.com"
+// export const IF="http://localhost:5000/images/"
 
-// // export const URL = "https://tublog-backend.onrender.com"
-// export const IF = "https://tublog-backend.onrender.com/images/"
+// export const URL = "https://tublog-backend.onrender.com"
+export const IF = "https://tublog-backend.onrender.com/images/"
 
-export const URL = 'http://localhost:5000'; // Update the backend URL to your local server.
-export const IF = 'http://localhost:5000/images/'; // Update the image URL as well.
+// export const URL = 'http://localhost:5000'; // Update the backend URL to your local server.
+// export const IF = 'http://localhost:5000/images/'; // Update the image URL as well.
