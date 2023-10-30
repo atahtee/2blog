820660fd7b47e77c8528f19c0b058f67fb425e02
atahtee <atatisam14@gmail.com>
2023-10-30 22:50:38 +0300

    Added views schema

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors(
     {
-    origin: 'https://2blog-atahtee.vercel.app',
+    origin: 'http://localhost:5173',
     methods: ["POST", "GET", "DELETE"],
     credentials:true}
     ))
