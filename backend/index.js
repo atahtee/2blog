3db4cfb7441e4400f17aab4f1e019112fda66cbb
atahtee <atatisam14@gmail.com>
2023-10-31 21:11:30 +0300

    Changed url

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors(
     {
-    origin: 'http://localhost:5173',
+    origin: 'https://2blog-atahtee.vercel.app/',
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials:true}
     ))
