67fe260ede83f3416b95f5ec097d4dffee941483
atahtee <atatisam14@gmail.com>
2023-10-31 12:38:29 +0300

    Changed hosting

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,7 +1,4 @@
 export const URL="https://tublog-backend.onrender.com"
-// export const IF="http://localhost:5000/images/"
-
-// export const URL = "https://tublog-backend.onrender.com"
 export const IF = "https://tublog-backend.onrender.com/images/"
 
 // export const URL = 'http://localhost:5000'; // Update the backend URL to your local server.
