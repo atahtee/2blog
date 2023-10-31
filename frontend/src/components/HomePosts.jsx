15d5f10414a76873a5d80286c3aec926e5f3a314
atahtee <atatisam14@gmail.com>
2023-10-31 11:22:05 +0300

    Made the site responsive

--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,33 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { IF } from '../url';
 import '../styles/font.css';
 
 const HomePosts = ({ post }) => {
-
   return (
     <div className='w-full flex mt-8 space-x-4 '>
-
-      {/*left */}
-      <div className='w-[35%] h-[200px] flex justify-center items-center border border-gray-200 p-4 rounded-lg shadow-lg bg-white transition transform hover:scale-105'>
-        <img src={IF + post.photo} alt="" className='h-full w-full object-cover' />
-      </div>
-
-      {/*right*/}
-      <div className='flex flex-col w-[65%] border border-gray-200 p-4 rounded-lg shadow-lg bg-white transition transform hover:scale-105'>
-        <div className='flex items-center justify-between'>
-          <h1 className='text-2xl font-bold mb-2'>
-            {post.title}
-          </h1>
-          {/* <i className='fa fa-eye text-blue-500'></i> */}
-          {/* <p>{viewCount}</p> */}
+      {/* Shared Container with Hover Effect */}
+      <div className='w-full flex flex-col md:flex-row hover:bg-gray-100 transition transform hover:scale-105'>
+        {/* Left (Image) */}
+        <div className='w-full md:w-[35%] h-[250px] p-4 rounded-lg shadow-lg bg-white'>
+          <img src={IF + post.photo} alt="" className='h-full w-full object-cover' />
         </div>
-        <div className='flex items-center justify-between mb-2 text-gray-500 text-sm'>
-          <p>@{post.username}</p>
-          <p>{new Date(post.updatedAt).toDateString()}</p>
+
+        {/* Right (Post Details) */}
+        <div className='w-full md:w-[65%] h-[250px] p-4 rounded-lg shadow-lg bg-white'>
+          <div className='flex items-center justify-between'>
+            <h1 className='text-2xl font-bold mb-2'>
+              {post.title}
+            </h1>
+          </div>
+          <div className='flex items-center justify-between mb-2 text-gray-500 text-sm'>
+            <p>@{post.username}</p>
+            <p>{new Date(post.updatedAt).toDateString()}</p>
+          </div>
+          <p className='text-sm md:text-base text-gray-700'>
+            {post.desc.slice(0, 400)}<span className='text-blue-500 cursor-pointer hover:underline'>...Read more</span>
+          </p>
         </div>
-        <p className='text-sm md:text-base text-gray-700'>
-          {post.desc.slice(0, 200)}<span className='text-blue-500 cursor-pointer hover:underline'>...Read more</span>
-        </p>
       </div>
     </div>
   );
